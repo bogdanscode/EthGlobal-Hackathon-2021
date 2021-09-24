74070e210c2cec5d82289f06118965a6b6df93f5
Bogdan Manzar <83293014+bogdanscode@users.noreply.github.com>
2021-09-24 08:26:29 -0700

    Create pricefeed.sol

---
+++ pricefeed.sol
@@ -0,0 +1,35 @@
+/*
+this is a chainlink oracle that will check of the price of the token that you want to trade is at the right price to buy
+*/
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0xcBb98864Ef56E9042e7d2efef76141f15731B82f);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
